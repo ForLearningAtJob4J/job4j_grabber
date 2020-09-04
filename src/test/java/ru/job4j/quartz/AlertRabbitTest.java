ce49be5d4026ac7494e195818bddaaa3979687ba
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-09-04 21:51:10 +0700

    7.3.6.1.1. Job c параметрами [#336839] UPD5

---
+++ src/test/java/ru/job4j/quartz/AlertRabbitTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.quartz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlertRabbitTest {
+
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+}
