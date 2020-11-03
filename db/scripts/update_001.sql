2b11286c362e65e864af0227364b4bb9b8d2084c
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2020-11-03 07:23:51 +0700

    7.3.6.4. Схема таблицы Post [#336848] UPD

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -2,7 +2,7 @@ create table if not exists post (
    id serial primary key not null,
    name varchar(1024),
    text text,
-   link varchar(1024),
+   link varchar(1024) UNIQUE,
    authorLink varchar(1024),
    created timestamp
 );
